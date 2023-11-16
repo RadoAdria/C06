17be9e7522836966d8dfabc61cabc5a7fe055e8c
RadoAdria <mdzbmazarin@gmail.com>
2023-11-16 14:09:21 +0100

    ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*
+                */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rado andria   <adresse@ecole.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 12:11:30 by rado andria       #+#    #+#             */
+/*   Updated: 2023/09/29 12:11:30 by rado andria      ###   ########.fr       */
+/*
+                */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	ft_putstr(argv[argc-1]);
+	return (0);
+}
